225c542131838ad6f7623f810ccba5036b41608b
James Warburton <jhwarby2012@outlook.com>
2025-11-18 10:53:05 +0000

    move navbar to components, add support for dropdown menu

--- navbar.ts
+++
@@ -1,4 +0,0 @@
-export default [
-	{ label: "Home", href: "/" },
-	{ label: "Keybinds", href: "/keybinds" },
-]
---
+++ src/components/navbar.ts
@@ -0,0 +1,11 @@
+export default [
+	{ label: "Home", href: "/" },
+	{ label: "Getting Started", href: "/guides/installation" },
+	// { label: "Roles", href: "/roles"},
+	{
+		label: "Gamemodes", children: [
+			{ label: "Bed Wars", href: "/gamemodes/bedwars/overview" }
+		]
+	},
+	{ label: "Keybinds", href: "/keybinds" },
+]
